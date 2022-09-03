ae322962e4ec5b44b95a3c6a103abf071ba84b20
newton chakma <newtonchakma304@gmail.com>
2022-09-03 11:45:33 +0600

    user callection aded

---
+++ userCollection/userCollects.js
@@ -0,0 +1,74 @@
+const user =[
+    {
+      id: "6312e76c4d8edc551bdc1020",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Becker Salazar",
+      gender: "male",
+      contact: "+1 (845) 415-3521",
+      address: "789 Wyckoff Street, Lund, Guam, 6604"
+    },
+    {
+      id: "6312e76c7efd5aee0ef33cde",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Katie Curtis",
+      gender: "female",
+      contact: "+1 (942) 425-3090",
+      address: "967 Lefferts Place, Gibbsville, North Dakota, 4954"
+    },
+    {
+      id: "6312e76c05d6cdbcfa2a6398",
+      picture: "http://placehold.it/32x32",
+      name: "Mitzi Frost",
+      gender: "female",
+      contact: "+1 (989) 506-2759",
+      address: "686 Essex Street, Kapowsin, South Carolina, 7874"
+    },
+    {
+      id: "6312e76cd8c32b8c21a5b664",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Soto Cooke",
+      gender: "male",
+      contact: "+1 (973) 425-3605",
+      address: "931 Degraw Street, Kenmar, Wisconsin, 7308"
+    },
+    {
+      id: "6312e76c2aac28f6ad0004ed",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Welch Howe",
+      gender: "male",
+      contact: "+1 (825) 470-2210",
+      address: "886 Beaver Street, Carrsville, Indiana, 5764"
+    },
+    {
+      id: "6312e76c89c54450ed13e39f",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Lopez Coleman",
+      gender: "male",
+      contact: "+1 (846) 411-3184",
+      address: "928 Vermont Street, Canoochee, Maryland, 5637"
+    },
+    {
+      id: "6312e76c7b2f03ee40cd7238",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Mcmahon Travis",
+      gender: "male",
+      contact: "+1 (955) 558-2187",
+      address: "435 Revere Place, Coaldale, Utah, 4376"
+    },
+    {
+      id: "6312e76c88ab006af0986ac0",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Rojas Joseph",
+      gender: "male",
+      contact: "+1 (997) 600-2494",
+      address: "492 Adelphi Street, Homestead, Oregon, 9766"
+    },
+    {
+      id: "6312e76c73bffae3fa6e1b97",
+      photoUrl: "http://placehold.it/32x32",
+      name: "Carissa Mccarty",
+      gender: "female",
+      contact: "+1 (979) 541-2224",
+      address: "860 Furman Street, Gerber, Tennessee, 656"
+    }
+  ]
