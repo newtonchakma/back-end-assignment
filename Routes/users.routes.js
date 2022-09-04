330ec48e7c03aa1e3ca63c8c51f34ae9c472cf6b
newton chakma <newtonchakma304@gmail.com>
2022-09-04 18:21:37 +0600

    all api added

--- Routes/users.routes.js
+++ Routes/users.routes.js
@@ -6,7 +6,10 @@ const router = express.Router();
 /* router
 .router("/") */
 router.get('/all',usersControllers.getAllUsers);
-router.get(("/random"),usersControllers.getRandomUser);
+router.get("/random",usersControllers.getRandomUser);
+router.post("/save",usersControllers.saveAUser);
+router.patch("/update/:id",usersControllers.updateUser);
+router.delete("/delete/:id",usersControllers.deleteAUser);
 
 
 
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -12,4 +12,44 @@ let users = require('./../users.json');
 
  module.exports.getRandomUser = (req,res)=>{
     res.send(users[Math.floor(Math.random() * users.length)])
-} 
+}
+
+module.exports.saveAUser = (req,res)=>{
+  const data =req.body;
+  const {id, name, contact, address, gender, photoUrl} =data;
+
+  id && gender && contact && address && photoUrl && name ? users.push(data) && res.send(users)
+   : 
+   res.send('invalid data')
+}
+
+module.exports.updateUser = (req,res)=>{
+  const id = req.params.id;
+  const newData = req.body;
+  const selected = users.find(user => user.id === Number(id));
+  if(!selected){
+    res.send ('user not found!!')
+  }else if(selected && newData){
+    const {id, name, gender,contact,address,photoUrl} = newData;
+    id?selected.id = id:selected.id = selected.id
+    name? selected.name = name :selected.name = selected.name
+    gender? selected.gender = gender: selected.gender = selected.gender
+    contact? selected.contact = contact: selected.contact
+    address? selected.address = address: selected.address
+    photoUrl? selected.photoUrl = photoUrl: selected.photoUrl
+    res.send(selected)
+  }
+
+}
+
+  // delete a user
+  module.exports.deleteAUser = (req, res)=>{
+    const Id = req.params.id;
+    const data = users.findIndex(user =>user.id === Number(Id));
+    data === -1
+    ?
+    res.send('user not found')
+    :
+    users.splice(data, 1) && res.send(users)
+  }
+
